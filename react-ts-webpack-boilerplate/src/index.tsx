426a810f2a7a32367ae158249e21f1949b306fdb
Zhang Mo <1780858127@qq.com>
2024-09-08 16:51:18 +1000

    Resolved merge conflicts

--- react-ts-webpack-boilerplate/src/index.tsx
+++ react-ts-webpack-boilerplate/src/index.tsx
@@ -1,11 +1,11 @@
-/** @format */
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") );
 root.render(
   <React.StrictMode>
     <Router>
@@ -13,3 +13,4 @@ root.render(
     </Router>
   </React.StrictMode>,
 );
+
