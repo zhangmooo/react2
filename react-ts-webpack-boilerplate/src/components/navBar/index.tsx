9319ee099e89a4e4894d8acc6cee8df0f2b8dd0a
DaHuo-Melbourne <huodamelbourne@gmail.com>
2024-09-26 21:43:31 +1000

    feat: typo is fixed

--- react-ts-webpack-boilerplate/src/components/navBar/index.tsx
+++ react-ts-webpack-boilerplate/src/components/navBar/index.tsx
@@ -24,7 +24,7 @@ const data = [
   },
   {
     label: 'PORTFOLIO',
-    to: '/portfolito',
+    to: '/portfolio',
   },
   {
     label: 'RESUME',
--- react-ts-webpack-boilerplate/src/components/pageHeadercontent/index.tsx
+++ react-ts-webpack-boilerplate/src/components/pageHeadercontent/index.tsx
@@ -1,10 +1,15 @@
 import './styles.css';
 import React from 'react';
 
-const PageHeaderContent: React.FC<{ headerText: React.ReactNode; icon: React.ReactNode }> = (
-  props,
-) => {
+const PageHeaderContent: React.FC<{
+  headerText: React.ReactNode;
+  icon: React.ReactNode;
+}> = (props) => {
   const { headerText, icon } = props;
+
+  console.log('11111');
+  console.log(headerText);
+  console.log('22222');
 
   return (
     <div className="wrapper">
